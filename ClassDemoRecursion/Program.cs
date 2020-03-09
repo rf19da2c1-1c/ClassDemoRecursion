c2a8123deb27389a0d034ec8bc52a7d52442774e
Peter Levinsky <pele@easj.dk>
2020-03-09 13:24:03 +0100

    Add project files.

---
+++ ClassDemoRecursion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursionWorker worker = new RecursionWorker();
+            worker.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassDemoRecursion/RecursionWorker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassDemoRecursion
+{
+    class RecursionWorker
+    {
+        public void Start()
+        {
+            // Recursion1(17);
+            
+            for (int i = 1; i <= 12; i++)
+            {
+                counter = 0;
+                long f = Fibunacci(i);
+                Console.WriteLine($"Fibunacci {i} Antal {counter} res {f}");
+            }
+
+        }
+
+        private static long counter = 0;
+
+
+        public void Recursion1(int i)
+        {
+            // stop
+            if (i == 0)
+                return; // stopper
+
+            // gøre et eller andet
+            Console.WriteLine(i);
+
+            // kalde sig selv - gerne med et mindre problem
+            Recursion1(i - 1);
+        }
+
+        public long Fibunacci(int i)
+        {
+            counter++;
+
+            // stop
+            if (i == 1 || i == 2)
+                return 1;
+
+            
+            return Fibunacci(i - 1) + Fibunacci(i - 2);
+        }
+    }
+}
